6d3b2df3935f034a51c19e3c799c8e3d4583ee42
Radhika Gupta <rawdhikagupta@gmail.com>
2024-02-23 18:22:20 +0530

    createGrammarRules() and createHashTable() added

--- parser/parser.h
+++ parser/parser.h
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "linkedList.c"
+#define NUMRULES 87
 
-void createGrammar(FILE * fp)
-{
-
-}
-
+ruleLL * createGrammar(FILE * fp,hash_ele * hash_table); 
+int calculateHash(char *word); 
+hash_ele * create_hashTable(); 
